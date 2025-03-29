312f14e3fe37c26ecd6786a333af3443d591ba2f
asa129 <asa.jad1290@gmail.com>
2025-03-29 13:57:51 +0900

    memo化

--- src/components/atoms/button/PrimaryButton.tsx
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -1,12 +1,13 @@
-import { FC } from "react";
+import { FC, memo } from "react";
 import { Button } from "@chakra-ui/react";
 
 type Props = {
   children: string;
 };
 
-export const PrimaryButton: FC<Props> = (props) => {
+export const PrimaryButton: FC<Props> = memo((props) => {
   const { children } = props;
+
   return (
     <>
       <Button colorScheme="teal" m={4}>
@@ -14,4 +15,4 @@ export const PrimaryButton: FC<Props> = (props) => {
       </Button>
     </>
   );
-};
+});
